b7e2fea7a32ed9a8dbb343d5de20b3bfa90d5434
lelandpaul <me@lelandpaul.com>
2018-07-01 21:23:50 -0400

    Node class

--- mtree.py
+++ mtree.py
@@ -7,3 +7,23 @@ Provides the MTree class, which instantiates multidominant trees made up of
 the Node class.
 
 """
+
+
+class Node(object):
+
+	def __init__(self,
+				name,
+				mother = None,
+				head = None,
+				child = None):
+		self.name = name
+		self.mother = mother
+		self.head = head
+		self.child = child
+
+	def isTerminal(self):
+		return(not (head and child))
+
+	def isRoot(self):
+		return(not mother)
+
