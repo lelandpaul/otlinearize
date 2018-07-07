f4c95c80c1cbbb9ef01f925948288427f9b02bc9
lelandpaul <me@lelandpaul.com>
2018-07-07 12:31:48 -0400

    Gen class

--- gen.py
+++ gen.py
@@ -3,8 +3,9 @@
 
 """
 
-Provides functions for iterating over output linearizations given a particular tree
-input.
+Provides the Gen class, along with functions for iterating over output
+linearizations given a particular tree input.
+
 
 """
 
@@ -19,3 +20,16 @@ def gen_strings(tree, null_phon = {}, spaces = False):
 		yield ''.join(perm)
 
 
+class Gen:
+
+	def __init__(self, function = gen_strings):
+		self.function = gen_strings
+		self.dictionary = dict()
+
+	def __call__(self, inp):
+		# yield the precreated values in the dictionary
+		# otherwise, build them and then yield them
+		yield from self.dictionary.get(inp, list(self.function(inp)))
+
+	def __getitem__(self, inp):
+		yield from self.dictionary[inp]
