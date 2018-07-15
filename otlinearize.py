deeac8206772495347557230ccaca22dfc92483f
lelandpaul <me@lelandpaul.com>
2018-07-14 23:07:25 -0400

    Made an interface with docopt

--- otlinearize.py
+++ otlinearize.py
@@ -1,5 +1,24 @@
 #! /usr/bin/python
 
+
+"""OT Linearization.
+
+Usage:
+    otlinearize.py [options]
+    otlinearize.py tableau [options] <tree>
+    otlinearize.py typology [options] <trees>...
+    otlinearize.py typology [options] -f <treelist>
+
+Options:
+    -h, --help	Show this screen.
+    --version	Show version.
+    -a, --all	For tableau: output all candidates (not just contenders).
+    --latex	Output in LaTeX format (as opposed to ASCII).
+    --alpha=NODE	Use the default constraints, but specify HF-alpha.
+"""
+
+
+from docopt import docopt
 from itertools import permutations
 
 from bin.mtree import *
@@ -7,29 +26,56 @@ from bin.gen import *
 from bin.con import *
 from bin.tableau import *
 
-basic = parseTreeFile('trees/basic.txt')
-basicmv = parseTreeFile('trees/basic-mvnt.txt')
-headmv = parseTreeFile('trees/basic-head.txt')
-treelist = [basic, basicmv, headmv]
+if __name__ == '__main__':
 
-antisym = Antisymmetry()
-hf = HeadFinality()
-hfbp = HeadFinality(alpha='BP')
-conlist = [antisym,hf,hfbp]
+	args = docopt(__doc__,version='OTLinearize 1.0')
+
+	# Build our Con:
+	conlist = [ Antisymmetry(),
+				HeadFinality(),
+				HeadFinality(alpha = 'BP' if not args['--alpha']
+										  else args['--alpha']),
+				]
+
+	if args['tableau']:
+		# We're making a single tableau; get the tree.
+		tree = parseTreeFile(args['<tree>'])
+
+		# now build the tableau:
+		output = Tableau(tree, conlist)
+
+		# Output appropriately:
+		if args['--latex']:
+			print(output.print_tabular(include_bounded=args['--all']))
+		else:
+			print(output.print_ascii(include_bounded=args['--all']))
 
 
-outputs = list(gen_strings(basic))
+	elif args['typology']:
+		# We're making a typology. Either we've been given a list of tree files
+		# directly, or we need to parse one.
 
-v_anti = {o: antisym(basic,o) for o in outputs}
-v_hf = {o: hf(basic,o) for o in outputs}
-v_hfa = {o: hfbp(basic,o) for o in outputs}
+		if args['<trees>']:
+			trees = args['<trees>']
+		elif args['<treelist>']:
+			with open(args['<treelist>'],'r') as treef:
+				trees = treef.read()
+				trees = trees.splitlines()
+		print(trees)
+		treelist = [parseTreeFile(t) for t in trees]
 
+		# Make our typology:
+		output = Typology(treelist, conlist)
 
-test_tab = Tableau(basic,conlist)
+		# Output appropriately:
+		if args['--latex']:
+			print(output.print_tabular())
+		else:
+			print(output.print_ascii())
 
-print(test_tab.print_ascii(True))
-
-test_typ = Typology(treelist,conlist)
-
-# print(test_typ)
+	
+	else:
+		# No command, just freak out
+		print(__doc__)
+		quit()
 
