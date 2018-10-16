35cca172c675002899ec80b6ddcd7b8c5e028cfb
lelandpaul <me@lelandpaul.com>
2018-10-16 15:05:57 -0400

    RollUpHeadEmpty

--- test/test_trees_from_paper.py
+++ test/test_trees_from_paper.py
@@ -7,7 +7,8 @@ from bin import *
 # Load trees
 @pytest.fixture(params = ["BaseGenSpec",
 						  "MovedSpec",
-						  "RollUpHead",])
+						  "RollUpHead",
+						  "RollUpHeadEmpty",])
 def tree(request):
 	t = mtree.parseTreeFile('trees/paper/' + request.param + '.txt')
 	return(t)
@@ -17,7 +18,8 @@ def tree(request):
 @pytest.mark.parametrize("result_dict", [
 	{ "BaseGenSpec": "[A2 [C1 C0 ] [A1 [B1 B0 ] A0 ] ]",
 	  "MovedSpec":   "[A2 [C1 C0 ] [A1 [B1 [C1 C0 ] B0 ] A0 ] ]",
-	  "RollUpHead": "[A1 [B1 [C1 C0 ] [B C0 B0 ] ] [A [B C0 B0 ] A0 ] ]"
+	  "RollUpHead": "[A1 [B1 [C1 C0 ] [B C0 B0 ] ] [A [B C0 B0 ] A0 ] ]",
+	  "RollUpHeadEmpty": "[A1 [B1 [E1 [C1 C0 ] [E C0 E0 ] ] [B [E C0 E0 ] B0 ] ] [A [B [E C0 E0 ] B0 ] A0 ] ]"
 	}])
 def test_tree_bracket_string(tree, result_dict):
 	assert tree.bracket_string == result_dict[tree.name]
