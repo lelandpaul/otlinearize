d911470fcf3a0193c25b12e8c9d11b8662bd3248
lelandpaul <me@lelandpaul.com>
2018-10-16 14:47:40 -0400

    Test structure for MovedSpec

--- test/test_trees_from_paper.py
+++ test/test_trees_from_paper.py
@@ -10,6 +10,10 @@ def tree_BaseGenSpec():
 	t = mtree.parseTreeFile('trees/paper/BaseGenSpec.txt')
 	return(t)
 
+@pytest.fixture
+def tree_MovedSpec():
+	t = mtree.parseTreeFile('trees/paper/MovedSpec.txt')
+	return(t)
 
 
 
@@ -17,6 +21,7 @@ def tree_BaseGenSpec():
 
 @pytest.mark.parametrize("tree, brackets", [
 	(tree_BaseGenSpec(), "[A2 [C1 C0 ] [A1 [B1 B0 ] A0 ] ]"),
+	(tree_MovedSpec(), "[A2 [C1 C0 ] [A1 [B1 [C1 C0 ] B0 ] A0 ] ]"),
 	])
 def test_tree_bracket_string(tree, brackets):
 	assert tree.bracket_string == brackets
