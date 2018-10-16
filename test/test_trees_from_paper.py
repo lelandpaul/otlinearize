9dec3542f0c39578338d0c7b262df7afae4a9d5a
lelandpaul <me@lelandpaul.com>
2018-10-16 14:54:05 -0400

    Restructured to have only one tree fixture

--- test/test_trees_from_paper.py
+++ test/test_trees_from_paper.py
@@ -5,23 +5,17 @@ from bin import *
 
 
 # Load trees
-@pytest.fixture
-def tree_BaseGenSpec():
-	t = mtree.parseTreeFile('trees/paper/BaseGenSpec.txt')
+@pytest.fixture(params = ["BaseGenSpec.txt",
+						  "MovedSpec.txt",])
+def tree(request):
+	t = mtree.parseTreeFile('trees/paper/' + request.param)
 	return(t)
-
-@pytest.fixture
-def tree_MovedSpec():
-	t = mtree.parseTreeFile('trees/paper/MovedSpec.txt')
-	return(t)
-
-
 
 # Test that trees have been loaded correctly
 
-@pytest.mark.parametrize("tree, brackets", [
-	(tree_BaseGenSpec(), "[A2 [C1 C0 ] [A1 [B1 B0 ] A0 ] ]"),
-	(tree_MovedSpec(), "[A2 [C1 C0 ] [A1 [B1 [C1 C0 ] B0 ] A0 ] ]"),
-	])
-def test_tree_bracket_string(tree, brackets):
-	assert tree.bracket_string == brackets
+@pytest.mark.parametrize("result_dict", [
+	{ "BaseGenSpec": "[A2 [C1 C0 ] [A1 [B1 B0 ] A0 ] ]",
+	  "MovedSpec":   "[A2 [C1 C0 ] [A1 [B1 [C1 C0 ] B0 ] A0 ] ]",
+	}])
+def test_tree_bracket_string(tree, result_dict):
+	assert tree.bracket_string == result_dict[tree.name]
