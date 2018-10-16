6eda1ab5038a7bd7f8eeae5bb62e771e991a96dc
lelandpaul <me@lelandpaul.com>
2018-10-16 15:09:32 -0400

    LongHeadEmpty

--- test/test_trees_from_paper.py
+++ test/test_trees_from_paper.py
@@ -8,7 +8,8 @@ from bin import *
 @pytest.fixture(params = ["BaseGenSpec",
 						  "MovedSpec",
 						  "RollUpHead",
-						  "RollUpHeadEmpty",])
+						  "RollUpHeadEmpty",
+						  "LongHeadEmpty",])
 def tree(request):
 	t = mtree.parseTreeFile('trees/paper/' + request.param + '.txt')
 	return(t)
@@ -19,7 +20,8 @@ def tree(request):
 	{ "BaseGenSpec": "[A2 [C1 C0 ] [A1 [B1 B0 ] A0 ] ]",
 	  "MovedSpec":   "[A2 [C1 C0 ] [A1 [B1 [C1 C0 ] B0 ] A0 ] ]",
 	  "RollUpHead": "[A1 [B1 [C1 C0 ] [B C0 B0 ] ] [A [B C0 B0 ] A0 ] ]",
-	  "RollUpHeadEmpty": "[A1 [B1 [E1 [C1 C0 ] [E C0 E0 ] ] [B [E C0 E0 ] B0 ] ] [A [B [E C0 E0 ] B0 ] A0 ] ]"
+	  "RollUpHeadEmpty": "[A1 [B1 [E1 [C1 C0 ] [E C0 E0 ] ] [B [E C0 E0 ] B0 ] ] [A [B [E C0 E0 ] B0 ] A0 ] ]",
+	  "LongHeadEmpty": "[E1 [A2 [B1 B0 ] [A1 [C1 C0 ] A0 ] ] [E A0 E0 ] ]",
 	}])
 def test_tree_bracket_string(tree, result_dict):
 	assert tree.bracket_string == result_dict[tree.name]
