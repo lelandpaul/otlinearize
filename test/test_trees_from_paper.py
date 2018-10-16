b4d4f157dda4957944749fffd86f09200bd74f19
lelandpaul <me@lelandpaul.com>
2018-10-16 14:36:58 -0400

    Testing the structure of BaseGenSpec

--- test/test_trees_from_paper.py
+++ test/test_trees_from_paper.py
@@ -2,3 +2,21 @@
 
 import pytest
 from bin import *
+
+
+# Load trees
+@pytest.fixture
+def tree_BaseGenSpec():
+	t = mtree.parseTreeFile('trees/paper/BaseGenSpec.txt')
+	return(t)
+
+
+
+
+# Test that trees have been loaded correctly
+
+@pytest.mark.parametrize("tree, brackets", [
+	(tree_BaseGenSpec(), "[A2 [C1 C0 ] [A1 [B1 B0 ] A0 ] ]"),
+	])
+def test_tree_bracket_string(tree, brackets):
+	assert tree.bracket_string == brackets
