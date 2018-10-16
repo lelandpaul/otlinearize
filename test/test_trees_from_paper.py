7a8f8001be9d407aa615aa65883afa76410169ca
lelandpaul <me@lelandpaul.com>
2018-10-16 15:17:00 -0400

    HighHead

--- test/test_trees_from_paper.py
+++ test/test_trees_from_paper.py
@@ -11,7 +11,8 @@ from bin import *
 						  "RollUpHead",
 						  "RollUpHeadEmpty",
 						  "LongHeadEmpty",
-						  "LongMovedSpec",])
+						  "LongMovedSpec",
+						  "HighHead",])
 def tree(request):
 	t = mtree.parseTreeFile('trees/paper/' + request.param + '.txt')
 	return(t)
@@ -26,6 +27,7 @@ def tree(request):
 	  "RollUpHeadEmpty": "[A1 [B1 [E1 [C1 C0 ] [E C0 E0 ] ] [B [E C0 E0 ] B0 ] ] [A [B [E C0 E0 ] B0 ] A0 ] ]",
 	  "LongHeadEmpty": "[E1 [A2 [B1 B0 ] [A1 [C1 C0 ] A0 ] ] [E A0 E0 ] ]",
 	  "LongMovedSpec": "[A2 [D1 D0 ] [A1 [B1 [C1 [D1 D0 ] C0 ] B0 ] A0 ] ]",
+	  "HighHead": "[A1 [B1 [C1 [D1 D0 ] C0 ] B0 ] [A B0 A0 ] ]",
 	}])
 def test_tree_bracket_string(tree, result_dict):
 	assert tree.bracket_string == result_dict[tree.name]
